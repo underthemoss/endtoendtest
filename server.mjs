1c234356743d0dd447f103cf500f30d1eb59a3af
underthemoss <will@equipmentshare.com>
2023-02-15 19:41:30 -0600

    added cors to server.mjs

--- server.mjs
+++ server.mjs
@@ -7,6 +7,14 @@ const port = 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.post("/business_units", async (req, res) => {
   const apiKey =
